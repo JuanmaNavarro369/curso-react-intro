6c755a79908a661726a9d5851c87bbe3fb382ff7
Juan Manuel Navarro González <juanma_2_@hotmail.com>
2024-01-22 19:29:24 +0100

    Teoría hasta la clase 4. Qué son los componentes y como pueden comunicarse entre ellos mediante props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './platzi.webp';
+import React from 'react';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { TodoButton } from './TodoButton'
+
+const defaultTodos = [
+  {text: "Hacer ejercicio", completed: false},
+  {text: "Estudiar", completed: true},
+  {text: "Volver a Edimburgo", completed: false},
+  {text: "Rejugar Resident Evil 4", completed: true}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <TodoCounter completed={27} total={69} />
+    <TodoSearch />
+
+    <TodoList>
+      {defaultTodos.map(todo => (
+        <TodoItem
+        key={todo.text}
+        text={todo.text}
+        completed={todo.completed}
+        />
+      ))}
+    </TodoList>
+
+    <TodoButton />
+
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/TodoCounter.js
@@ -0,0 +1,7 @@
+function TodoCounter (props) {
+    return (
+      <h1>Has completado {props.completed} de {props.total} Todo's</h1>
+    );
+  };
+
+  export { TodoCounter };
