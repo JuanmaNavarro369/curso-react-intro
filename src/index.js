52dd8f51878347e0ad867cab0e77e94fc1eef95d
Juan Manuel Navarro González <juanma_2_@hotmail.com>
2024-02-15 20:31:43 +0100

    Downgrade de React V 18.0.2 a V 17.0.2 y cambio general en la fuente de letras

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';  // 'react-dom/client' V 18.0.2
 import { App } from './App/App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App/>);             V 18.0.2
+
+ReactDOM.render(<App />, document.getElementById('root'));
